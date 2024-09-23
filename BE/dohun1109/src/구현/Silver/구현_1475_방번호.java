0bc06a13255605eab9f41cb2d24e6bccb73e1405
dohun1109 <dohun1109@naver.com>
2024-09-23 20:51:57 +0900

    09/23 1475 방번호

---
+++ BE/dohun1109/src/구현/Silver/구현_1475_방번호.java
@@ -0,0 +1,40 @@
+package 구현.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 구현_1475_방번호 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] ch = br.readLine().toCharArray();
+        int[] num = new int[10];
+
+        
+        int MIN = Integer.MIN_VALUE;
+
+        System.out.println(Arrays.toString(ch));
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i]  == '6'){
+                num[9]++;
+            }else {
+                num[(int)ch[i]-'0']++;
+            }
+        }
+
+        int nine = num[9]/2;
+        if (num[9]%2==1){
+            nine++;
+        }
+        num[9] = nine;
+
+        for (int i =0; i<10; i++){
+            MIN = Math.max(MIN,num[i]);
+        }
+        System.out.println(MIN);
+
+    }
+}
+
