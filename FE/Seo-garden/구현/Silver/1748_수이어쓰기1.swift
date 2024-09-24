66ab667373a6b415b8614e2fcfb53d9ff342e6c0
seo-garden <indian4589@naver.com>
2024-09-24 17:12:13 +0900

    Add: #1748

---
+++ FE/Seo-garden/구현/Silver/1748_수이어쓰기1.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let input = fIO.readInt()
+
+var i = 1
+var answer = 0
+
+while i <= input {
+    answer += input - i + 1
+    i *= 10
+}
+print(answer)
+
+//만약 입력이 120 이 들어 왔을 때
+//한자리수 : 1 ~ 9 : 9 * 1 = 9개
+//두자리수 : 10 ~ 99 : 99 - 10 + 1 : 90 * 2 = 180개
+//세자리수 : 100 ~ 120 : 120 - 100 + 1 : 21 * 3 = 63개 --> 9 + 180 + 63 = 252 자리수
