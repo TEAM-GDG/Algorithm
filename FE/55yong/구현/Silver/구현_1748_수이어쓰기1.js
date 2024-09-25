ef1e8fe5ac8925f729fa2c4439eec4caedac431f
Yongjun OH <132319467+55yong@users.noreply.github.com>
2024-09-25 21:10:44 +0900

    [BOJ] 수 이어 쓰기 1 (#30)
    * Add: #1748
    
    * Add: 확인용 console.log()줄 삭제

---
+++ FE/55yong/구현/Silver/구현_1748_수이어쓰기1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+let result = 0;
+
+for (let i = 1; i <= input; i *= 10) {
+  result += input - i + 1;
+}
+
+console.log(result);
+
+/**
+ * 입력값이 821인 경우
+ * input - i + 1이니옴
+ * 1   ~   9의 개수는 9 - 1 + 1 = 9개
+ * 10  ~  99의 개수는 99 - 10 + 1 = 90개
+ * 100 ~ 821의 개수는 821 - 100 + 1 = 722개로 계산할 수 있음
+ * 그리고 자릿수를 계산해서 자릿수만큼 곱해서 덧셈하면 됨
+ * (9 * 1) + (90 * 2) + (722 * 3) = 2355
+ */
