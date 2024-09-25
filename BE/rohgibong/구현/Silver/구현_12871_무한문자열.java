cc23029df870f24a6cde95953a2e45fa1ca6ad4e
rohgibong <rohgibong@gmail.com>
2024-09-25 13:28:05 +0900

    09/25 12871 무한문자열

--- BE/rohgibong/구현/Silver/구현_12871_무한문자열.java
+++ BE/rohgibong/구현/Silver/구현_12871_무한문자열.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Test4 {
+public class 구현_12871_무한문자열 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
