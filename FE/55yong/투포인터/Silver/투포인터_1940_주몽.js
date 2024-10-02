60df045922cbd35a572d71306e279eefa8176836
Yongjun OH <132319467+55yong@users.noreply.github.com>
2024-10-02 21:59:09 +0900

    [BOJ] 주몽 (#81)
    * Add: #1940
    
    * Add: #1940(2)
    
    ---------
    
    Co-authored-by: 서정원 <125368624+Seo-garden@users.noreply.github.com>

---
+++ FE/55yong/투포인터/Silver/투포인터_1940_주몽.js
@@ -0,0 +1,31 @@
+const filepath =
+  process.platform === "linux" ? "/dev/stdin" : "../../input.txt";
+const input = require("fs")
+  .readFileSync(filepath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input[0]); // 재료의 개수
+const M = Number(input[1]); // 갑옷을 만드는데 필요한 수
+const S = input[2].split(" ").sort((a, b) => a - b); // 재료 번호
+
+let answer = 0;
+let p1 = 0; // 포인터 1
+let p2 = N - 1; // 포인터 2
+
+while (p1 !== p2) {
+  // 포인터 1과 2가 만날때까지 반복
+  const result = Number(S[p1]) + Number(S[p2]);
+
+  if (result === M) {
+    answer++;
+  }
+
+  if (result > M) {
+    p2--;
+  } else {
+    p1++;
+  }
+}
+console.log(answer);
