0304c8f69d24be320070d86f059204609152969d
Yongjun OH <132319467+55yong@users.noreply.github.com>
2024-10-15 16:03:52 +0900

    Add: #11722 (#253)

---
+++ FE/55yong/DP/Silver/DP_11722_가장긴감소하는부분수열.js
@@ -0,0 +1,24 @@
+const filepath =
+  process.platform === "linux" ? "/dev/stdin" : "../../input.txt";
+const input = require("fs")
+  .readFileSync(filepath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = input[0];
+const A = input[1].split(" ").map(Number);
+
+const dp = new Array(N).fill(0);
+
+for (let i = 0; i < N; i++) {
+  let max = 0;
+  for (let j = 0; j < i; j++) {
+    if (A[i] < A[j] && dp[j] > max) {
+      max = dp[j];
+    }
+  }
+  dp[i] = max + 1;
+}
+
+console.log(Math.max(...dp));
