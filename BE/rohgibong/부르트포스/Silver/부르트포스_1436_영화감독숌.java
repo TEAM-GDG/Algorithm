9859c22022f17490487f20fa55df36b21a5fce0f
GIbong Roh <119557561+rohgibong@users.noreply.github.com>
2024-10-28 18:14:28 +0900

    Add: #1436

---
+++ BE/rohgibong/부르트포스/Silver/부르트포스_1436_영화감독숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부르트포스_1436_영화감독숌 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 666;
+        int count = 0;
+
+        while(true){
+            if(String.valueOf(result).contains("666")){
+                count++;
+                if(count == N) break;
+            }
+            result++;
+        }
+        System.out.println(result);
+    }
+}
