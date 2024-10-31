4922a0aeb40455ca80b6d48e535f3d8695befa32
GIbong Roh <119557561+rohgibong@users.noreply.github.com>
2024-10-31 23:41:38 +0900

    Add: #11000

---
+++ BE/rohgibong/그리디/Gold/그리디_11000_강의실배정.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 그리디_11000_강의실배정 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] time = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            int start = time[i][0];
+            int end = time[i][1];
+
+            if(!pq.isEmpty() && pq.peek() <= start) {
+                pq.poll();
+            }
+            pq.add(end);
+        }
+        System.out.println(pq.size());
+    }
+}
