b79c20ff1b547864e0001fd4e0d5877e21f640ea
GIbong Roh <119557561+rohgibong@users.noreply.github.com>
2024-11-01 20:27:43 +0900

    Add: #1476 (#364)

--- BE/rohgibong/브루트포스/Silver/브루트포스_1065_한수.java
+++ BE/rohgibong/브루트포스/Silver/브루트포스_1065_한수.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 부르트포스_1065_한수 {
+public class 브루트포스_1065_한수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
